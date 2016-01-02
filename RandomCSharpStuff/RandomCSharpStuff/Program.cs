0ea6c2cb9ca09b464e63c302786330850867b24e
Jim <jrthom18@gmail.com>
2016-01-02 15:57:17 -0700

    Function to find minimum value in list of integers
    Function assumes list was initially sorted from min to max and then
    rotated (i.e. 1,2,3,4,5 would be rotated to 3,4,5,1,2)

--- RandomCSharpStuff/RandomCSharpStuff/Program.cs
+++ RandomCSharpStuff/RandomCSharpStuff/Program.cs
@@ -11,7 +11,44 @@ namespace RandomCSharpStuff
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<int> intList = new List<int>(){3,4,5,6,7,1,2};
+            Console.WriteLine(FindMin(intList));
+            
             Console.ReadKey();
+        }
+
+        public static int FindMin(List<int> inputList)
+        {
+            if (inputList.Count == 1)
+            {
+                return inputList[0];
+            }
+            else
+            {
+                int mid = 0;
+                int right = inputList[inputList.Count - 1];;
+                if (inputList.Count%2 == 0)
+                {
+                    mid = inputList[inputList.Count / 2 - 1];
+                }
+                else
+                {
+                    mid = inputList[inputList.Count / 2];
+                }
+                
+                List<int> sublist = new List<int>();
+                if (mid > right)
+                {
+                    sublist = inputList.GetRange(inputList.IndexOf(mid) + 1,
+                        inputList.Count - 1 - inputList.IndexOf(mid));
+                    return FindMin(sublist);
+                }
+                else
+                {
+                    sublist = inputList.GetRange(0, inputList.IndexOf(mid) + 1);
+                    return FindMin(sublist);
+                }
+            }
         }
     }
 }
