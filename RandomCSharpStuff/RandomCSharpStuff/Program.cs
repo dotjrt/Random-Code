e4097154fa8feea845d0ec44b2a9417c3cd97e06
Jim <jrthom18@gmail.com>
2016-01-04 13:22:05 -0700

    Wrote function for Lyft programming challenge question

--- RandomCSharpStuff/RandomCSharpStuff/Program.cs
+++ RandomCSharpStuff/RandomCSharpStuff/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Device.Location;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,6 +61,30 @@ namespace RandomCSharpStuff
                     return FindMin(sublist);
                 }
             }
+        }
+
+        // LYFT programming challenge:
+        // Calculate the detour distance between two different rides. Given four latitude / longitude pairs, 
+        // where driver one is traveling from point A to point B and driver two is traveling from point C 
+        // to point D, write a function (in your language of choice) to calculate the shorter of the detour 
+        // distances the drivers would need to take to pick-up and drop-off the other driver.
+
+        // ***Calculations assume driving straight line distances between coordinates***
+        // Driver 1 would drive from A to C (pick up driver 2) to D (drop off driver 2) and finally to B
+        // Driver 2 would drive from C to A (pick up driver 1) to B (drop off driver 1) and finally to D
+        // Both routes involve trips between A & C and B & D, so those distances can be eliminated from consideration
+        // Therefore, if distance(A,B) > distance(C,D), driver 1 would have the shorter overall detour route
+        // Otherwise, distance(C,D) > distance(A,B), so driver 2 would have the shorter overall detour route
+        public double ShortestDetour(GeoCoordinate pointA, GeoCoordinate pointB, GeoCoordinate pointC,
+            GeoCoordinate pointD)
+        {
+            if (pointA.GetDistanceTo(pointB) > pointC.GetDistanceTo(pointD))
+            {
+                //Driver 1 has the shorter route
+                return pointA.GetDistanceTo(pointC) + pointC.GetDistanceTo(pointD) + pointD.GetDistanceTo(pointB);
+            }
+            //Driver 2 has the shorter route
+            return pointC.GetDistanceTo(pointA) + pointA.GetDistanceTo(pointB) + pointB.GetDistanceTo(pointD);
         }
     }
 }
