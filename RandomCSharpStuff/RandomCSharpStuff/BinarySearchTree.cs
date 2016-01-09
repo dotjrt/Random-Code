2fe6cc5474845d465f36f754fbde2ec4dfe49715
Jim <jrthom18@gmail.com>
2016-01-08 22:38:33 -0700

    Starting to implement a binary search tree

---
+++ RandomCSharpStuff/RandomCSharpStuff/BinarySearchTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace RandomCSharpStuff
+{
+    public class BinarySearchTree
+    {
+        public BstLeaf Root;
+
+        public void Insert(BstLeaf root, int key)
+        {
+            if (key < root.Key)
+            {
+                if (root.Left == null)
+                {
+                    root.Left = new BstLeaf(key);
+                }
+
+                else
+                {
+                    root = root.Left;
+                    Insert(root, key);
+
+                }
+            }
+            else if (key > root.Key)
+            {
+                if (root.Right == null)
+                {
+                    root.Right = new BstLeaf(key);
+                }
+
+                else
+                {
+                    root = root.Right;
+                    Insert(root, key);
+                }
+            }
+        }
+
+        public void Traverse(BstLeaf root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            
+            Console.WriteLine(root.Key);
+            Traverse(root.Right);
+            Traverse(root.Left);
+        }
+    }
+}
---
+++ RandomCSharpStuff/RandomCSharpStuff/BstLeaf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RandomCSharpStuff
+{
+    public class BstLeaf
+    {
+        public int Key;
+        public BstLeaf Parent;
+        public BstLeaf Left;
+        public BstLeaf Right;
+
+        public BstLeaf(){}
+
+        public BstLeaf(int key)
+        {
+            Key = key;
+        }
+    }
+}
